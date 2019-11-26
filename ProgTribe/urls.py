4dfa4e91c191ba8cb1bf64cdb3db3748af70c46b
AntonAks <antonaks84@gmail.com>
2019-11-26 11:13:29 +0200

    add static files and index template

--- ProgTribe/urls.py
+++ ProgTribe/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, IndexPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('index/', IndexPageView.as_view(), name='index'),
+
 ]
