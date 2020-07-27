e5edfca4e9a864c211f24474122e1c9763d7c86c
AntonAks <antonaks84@gmail.com>
2020-07-27 20:57:49 +0300

    change default language + delete redundant files

--- WagSite/settings/base.py
+++ WagSite/settings/base.py
@@ -132,7 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'uk'
 
 LANGUAGES = [
     ('en', _('English')),
